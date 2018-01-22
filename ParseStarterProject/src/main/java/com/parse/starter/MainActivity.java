2611cf2254937e4f1d1f4621f163b7728e6f5c50
Andre Vieira Goncalves <andrevieira@brq.com>
2018-01-22 16:32:02 -0200

    filtros

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,7 +9,6 @@
 package com.parse.starter;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -33,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-      //teste com Parse
+      //add dados no Parse
       /*ParseObject pontuacao = new ParseObject("Pontuacao");
-      pontuacao.put("nome", "Maria");
-      pontuacao.put("pontos", 150 );
+      pontuacao.put("nome", "Luiza");
+      pontuacao.put("pontos", 7555 );
       pontuacao.saveInBackground(new SaveCallback() {
           @Override
           public void done(ParseException e) {
@@ -49,15 +48,16 @@ public class MainActivity extends AppCompatActivity {
       });*/
 
 
+      //Consulta de dados
       /*ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
-      consulta.getInBackground("zUAAYzbYsn", new GetCallback<ParseObject>() {
+      consulta.getInBackground("NVsD8EWQdE", new GetCallback<ParseObject>() {
           @Override
           public void done(ParseObject object, ParseException e) {
 
               if( e == null ){
-
-                object.put("pontos", 1200 );
-                object.saveInBackground();
+                  //Altera a pontuação
+                  object.put("pontos", 1150 );
+                  object.saveInBackground();
 
               }else{
                   Log.i("consultaObjeto", "Erro ao consultar objeto" + e.getMessage() );
@@ -69,14 +69,31 @@ public class MainActivity extends AppCompatActivity {
       ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
 
       //Aplicando filtros na listagem de objetos
+      // pode colocar varios filtros de uma vez
+
+      //campo mais o valor nesse caso maior que 800
       //filtro.whereGreaterThan("pontos", 800);
-      filtro.whereGreaterThanOrEqualTo("pontos", 800);
+
+      //igual ou maior que 800
+      //filtro.whereGreaterThanOrEqualTo("pontos", 800);
+
+      //menor que 500
       //filtro.whereLessThan("pontos", 500);
+
+      //termina com
       //filtro.whereEndsWith("nome", "ia");
+
+      //inicia com
       //filtro.whereStartsWith("nome", "Ja");
+
+      //ordem crescente
       //filtro.addAscendingOrder("pontos");
-      filtro.addDescendingOrder("pontos");
-      filtro.setLimit(1);
+
+      //ordem decrescene
+      //filtro.addDescendingOrder("pontos");
+
+      // o tanto de resultados que quer exibir
+      //filtro.setLimit(1);
 
 
       //Listar os dados
