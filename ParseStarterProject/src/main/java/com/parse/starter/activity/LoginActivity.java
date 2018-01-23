cf08d9ed45505876f0e37c1f809f592d3af0798c
Andre Vieira Goncalves <andrevieira@brq.com>
2018-01-23 13:53:54 -0200

    Tela de Cadastro

--- ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parse.starter.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastro(View view) {
+        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+        startActivity(intent);
     }
 }
